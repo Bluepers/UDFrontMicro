b9bc71df070da52f3b2ddd9a1f1bfd8c145c6ab5
Bluepers <132324963+Bluepers@users.noreply.github.com>
2023-06-08 17:47:42 +0900

    Pointless change to test github actions

--- app2/container/src/App.js
+++ app2/container/src/App.js
@@ -3,13 +3,13 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 const App = () => {
-    return (
-        <>
-            <h1>Hello!</h1>
-            <hr />
-            <MarketingApp />
-        </>
-    );
+  return (
+    <>
+      <h1>Hello!</h1>
+      <hr />
+      <MarketingApp />
+    </>
+  );
 };
 
 export default App;
