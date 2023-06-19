13ae7a9c3805db25b36a4f4e7e22bd572df2f64f
Bluepers <132324963+Bluepers@users.noreply.github.com>
2023-06-19 22:47:42 +0900

    Remove debug

--- app2/container/config/webpack.prod.js
+++ app2/container/config/webpack.prod.js
@@ -6,8 +6,6 @@ const packageJSON = require("../package.json");
 
 const domain = process.env.PRODUCTION_DOMAIN;
 
-console.log(`marketing@/${domain}/marketing/latest/remoteEntry.js`);
-
 /** @type {import('webpack').Configuration} */
 const prodConfig = {
     mode: "production",
