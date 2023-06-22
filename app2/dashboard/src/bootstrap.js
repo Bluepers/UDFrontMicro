1338437e83129303f980ed3fade86f129a689ef8
Bluepers <132324963+Bluepers@users.noreply.github.com>
2023-06-22 17:45:17 +0900

    Fix this. is undefined error ay runtime

--- app2/dashboard/src/bootstrap.js
+++ app2/dashboard/src/bootstrap.js
@@ -7,9 +7,12 @@
 import { createApp } from "vue";
 import Dashboard from "../components/Dashboard.vue";
 
+import PrimeVue from "primevue/config";
+
 const mount = (el) => {
     const app = createApp(Dashboard);
 
+    app.use(PrimeVue);
     app.mount(el);
 };
 
